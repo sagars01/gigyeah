b0a5ec5a831fc85b8939f3786472a82c9dde8ccc
Sagar <sagarmoysengupta@gmail.com>
2024-04-20 16:19:23 +0530

    to clear unused components

--- app/libs/components/landingPage/v2/LandingPageV2.tsx
+++
@@ -1,9 +0,0 @@
-export default function LandingPageV2() {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </>
-    )
-}
