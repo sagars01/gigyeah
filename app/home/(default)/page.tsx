e122b17055b0de71a5f2126af0478d1a7185827e
Sagar <sagarmoysengupta@gmail.com>
2024-05-03 18:55:55 +0530

    to add open graph image for the site

--- app/home/(default)/page.tsx
+++ app/home/(default)/page.tsx
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: 'Hire WithJessi: Simplest ATS for Tech Hiring',
-  description: 'Simplest ATS for startups',
-}
-
 import Hero from '../components/hero'
 import Features from '../components/features'
 import FeaturesBlocks from '../components/features-blocks'
@@ -10,6 +5,8 @@ import Testimonials from '../components/testimonials'
 import Newsletter from '../components/newsletter'
 
 export default function Home() {
+
+
   return (
     <>
       <Hero />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,43 @@
+
 import React from 'react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs';
-import Head from 'next/head';
+import { Metadata } from 'next';
+
+const domain = process.env.NEXT_PUBLIC_APP_DOMAIN as string;
+const imgUrl = domain + "/img/homepage/og_homt.png"
+
+
+export const metadata: Metadata = {
+  title: 'Hiring Made Simple',
+  description: 'Simplest ATS for recruiting software engineers',
+  openGraph: {
+    title: 'WithJessi',
+    description: 'The Simplest ATS to hire tech talent',
+    url: 'https://www.withjessi.com',
+    siteName: 'WithJessi',
+    images: [
+      {
+        url: imgUrl, // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: imgUrl, // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: 'View With Jessi',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
+
+
+
 
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
